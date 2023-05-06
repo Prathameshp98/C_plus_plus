9a05f491ca70cf484aaa4829f3d2fe4bfe423578
Prathamesh Patil <prathamesh.patil@ufaber.com>
2023-05-06 16:37:47 +0530

    initial commit

---
+++ Structure of c++ program/Comments/main.cpp
@@ -0,0 +1,21 @@
+/**************************************
+
+Author Prathamesh
+
+***************************************/
+
+#include <iostream>
+
+using namespace std;
+
+// this is a comment
+
+/*
+    This is a multi line comment
+
+*/
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
